5447acacfc99c742cb7b594c20765be5b2a9f439
darkhorse1997 <tanmoydas1997@gmail.com>
2021-03-15 00:50:51 +0530

    Added proptype validation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledButton = styled.button`
-    color: Blue;
-`
-const Button = ({name, clickHandler}) => {
-    
-    return(
-    <StyledButton onClick = {clickHandler}>
-        {name}
-        
-    </StyledButton>
-    )};
+  color: Blue;
+`;
+const Button = ({ name, clickHandler }) => {
+  return <StyledButton onClick={clickHandler}>{name}</StyledButton>;
+};
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default Button;
-
