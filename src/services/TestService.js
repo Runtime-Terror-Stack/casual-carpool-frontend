8acd2dddded0510c424e8f40012764cc177fbf40
darkhorse1997 <tanmoydas1997@gmail.com>
2021-03-15 00:50:37 +0530

    Formatted by prettier

--- src/services/TestService.js
+++ src/services/TestService.js
@@ -1,47 +1,47 @@
-import Axios from 'axios';
+import Axios from "axios";
 
-const port = 6969;
+// const port = 6969;
 const baseUrl = "http://localhost:6969";
 const basePath = "bitches";
 
 const getAll = async () => {
-    const response = await Axios.get(`${baseUrl}/${basePath}`);
-    console.log(response.data)
-    return response.data;
-}
+  const response = await Axios.get(`${baseUrl}/${basePath}`);
+  console.log(response.data);
+  return response.data;
+};
 
-const getById =  async (id) => {
-    const id1 = 2;
-    const response = await Axios.get(`${baseUrl}/${basePath}/${id1}`);
-    console.log(response.data);
-    return response.data;
-}
+const getById = async (id) => {
+  const id1 = 2;
+  const response = await Axios.get(`${baseUrl}/${basePath}/${id1}`);
+  console.log(response.data);
+  return response.data;
+};
 
 const addValue = async (value) => {
-    const value1 =  {id: 4, name: "abhirup" };
-    const response =  await Axios.post(`${baseUrl}/${basePath}`, value1);
-    console.log(response.data);
-    return response.data;
-}
+  const value1 = { id: 4, name: "abhirup" };
+  const response = await Axios.post(`${baseUrl}/${basePath}`, value1);
+  console.log(response.data);
+  return response.data;
+};
 
-const updateValue =  async (value) => {
-    const value1 =  {id: 2, name: "abhirup" };
-    const response = await Axios.put(`${baseUrl}/${basePath}`, value1);
-    console.log(response.data);
-    return response.data;
-}
+const updateValue = async (value) => {
+  const value1 = { id: 2, name: "abhirup" };
+  const response = await Axios.put(`${baseUrl}/${basePath}`, value1);
+  console.log(response.data);
+  return response.data;
+};
 
 const deleteValue = async (id) => {
-    const id1 = 3;
-    const response =  await Axios.delete(`${baseUrl}/${basePath}/${id1}`);
-    console.log(response.data);
-    return response.data;
-}
+  const id1 = 3;
+  const response = await Axios.delete(`${baseUrl}/${basePath}/${id1}`);
+  console.log(response.data);
+  return response.data;
+};
 
 export default {
-    getAll,
-    getById,
-    addValue,
-    updateValue,
-    deleteValue
-}
+  getAll,
+  getById,
+  addValue,
+  updateValue,
+  deleteValue,
+};
