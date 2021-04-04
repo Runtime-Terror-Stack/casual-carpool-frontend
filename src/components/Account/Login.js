90a6d469836aca373f60feaf5fb240a44bbef5ae
darkhorse1997 <tanmoydas1997@gmail.com>
2021-04-05 01:46:35 +0530

    Completed Sign Up form UI

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -3,14 +3,19 @@ import {
   FormContainer,
   StyledInput,
   StyledLabel,
+  InputContainer,
+  StyledButton,
+  ButtonContainer,
+  StyledHeading,
+  StyledText,
 } from "./Login.styles";
 import React from "react";
 import PropTypes from "prop-types";
 
 const InputBox = (props) => {
   return (
-    <>
-      <StyledLabel for={props.id}>{props.text}</StyledLabel>
+    <InputContainer id={props.gridarea}>
+      <StyledLabel htmlFor={props.id}>{props.text}</StyledLabel>
       <StyledInput
         type="text"
         id={props.id}
@@ -20,20 +25,31 @@ const InputBox = (props) => {
         minlength="4"
         maxlength="8"
       />
-    </>
+    </InputContainer>
   );
 };
 
 const Login = () => {
   return (
     <LoginContainer>
+      <StyledHeading>Sign Up</StyledHeading>
       <FormContainer>
-        <InputBox id="fname" text="First Name" />
-        <InputBox id="lname" text="Last Name" />
-        <InputBox id="phone" text="Mobile Number" />
+        <InputBox id="fname" text="First Name" gridarea="firstname" />
+        <InputBox id="lname" text="Last Name" gridarea="lastname" />
+        <InputBox id="phone" text="Mobile Number" gridarea="phonenum" />
+        <ButtonContainer>
+          <StyledButton>Submit</StyledButton>
+        </ButtonContainer>
+        <StyledText>Already registered? </StyledText>
       </FormContainer>
     </LoginContainer>
   );
 };
 
 export default Login;
+
+InputBox.propTypes = {
+  id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  gridarea: PropTypes.string.isRequired,
+};
--- src/components/Account/Login.styles.js
+++ src/components/Account/Login.styles.js
@@ -1,9 +1,72 @@
 import styled from "styled-components";
 
-export const LoginContainer = styled.section``;
+export const StyledHeading = styled.h2``;
 
-export const FormContainer = styled.form``;
+export const LoginContainer = styled.section`
+  padding: 20px;
+  display: grid;
+  place-items: center;
+  background-color: whitesmoke;
+  border-radius: 10px;
+`;
 
-export const StyledInput = styled.input``;
+export const FormContainer = styled.form`
+  display: grid;
+  grid-template-areas:
+    "firstname lastname"
+    "phonenum phonenum"
+    "submit submit";
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+`;
 
-export const StyledLabel = styled.label``;
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-area: ${(props) => props.id};
+`;
+
+export const StyledInput = styled.input`
+  /* all: unset; */
+  margin: 10px 0;
+  outline: solid;
+  box-shadow: none;
+  /* border-radius: 5px; */
+  display: block;
+
+  &:focus {
+    outline: solid lightblue;
+    box-shadow: none;
+  }
+`;
+
+export const StyledLabel = styled.label`
+  color: black;
+  font-size: 0.9rem;
+`;
+
+export const ButtonContainer = styled.div`
+  grid-area: submit;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const StyledButton = styled.button`
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: 80%;
+  text-transform: uppercase;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const StyledText = styled.p`
+  display: block;
+  color: black;
+  font-size: 0.7rem;
+`;
