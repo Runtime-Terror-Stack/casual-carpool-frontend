2b05bdf6c65d135a5e387bae5257129e5c0d969d
darkhorse1997 <tanmoydas1997@gmail.com>
2021-04-07 01:22:37 +0530

    Added Login and Register component to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Login from "./components/Account/Login";
+// import OTPVerify from "./components/Account/OTPVerify";
 // import Button from "./components/Button/Button";
 // import TestService from "./services/TestService";
-import Login from "./components/Account/Login";
+import Register from "./components/Account/Register";
 
 function App() {
   return (
     <div className="App">
       <h1>Casual Carpool</h1>
+      <Register />
       <Login />
+      {/* <OTPVerify /> */}
     </div>
   );
 }
