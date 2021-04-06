81c81135c783f2a37a26cdd837e9f5404e3f67ea
darkhorse1997 <tanmoydas1997@gmail.com>
2021-04-07 02:13:17 +0530

    Fixed local image not showing

--- src/components/Button/Map.js
+++ src/components/Button/Map.js
@@ -1,7 +1,9 @@
-import React, { useRef,useEffect, useState } from "react";
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
+import markerimg from "../../image/marker.png";
+
 // const MyMap = ()=> {
 //     const [viewport, setViewport] = useState({
 //       latitude: 20.5937,
@@ -9,9 +11,9 @@ import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 //       zoom: 3.5,
 //       width: window.innerWidth,
 //       height: 0.75*window.innerHeight,
-      
+
 //     });
-  
+
 //     return (
 //       <ReactMapGL
 //       mapStyle={ "mapbox://styles/shuvro97/ckn27edc22thl17ms9epngrtx"}
@@ -24,46 +26,49 @@ import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 //     );
 //   }
 
-const MyMap = ()=>{
-    mapboxgl.workerClass = MapboxWorker;
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic2h1dnJvOTciLCJhIjoiY2pubjJ4bzA1MjI4bTNxb2pwcmV4OXE4byJ9.4XJJfg7_arK7gXzDMXEpBQ';
-    const mapContainer = useRef(null);
-    const markRef = useRef(null);
-    const [lng, setLng] = useState(88.4509);
-    const [lat, setLat] = useState(22.6964);
-    const [zoom, setZoom] = useState(18);
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-          container: mapContainer.current,
-          style: "mapbox://styles/shuvro97/ckn27edc22thl17ms9epngrtx",
-          center: [lng, lat],
-          zoom: zoom,
-          maxZoom:19 ,
-        });
-        const marker=new mapboxgl.Marker(markRef.current).setLngLat([88.4509,22.6964]).addTo(map);
-        new mapboxgl.Marker().setLngLat([88.4509,22.6964]).addTo(map);
-        map.on('move', () => {
-            setLng(map.getCenter().lng.toFixed(4));
-            setLat(map.getCenter().lat.toFixed(4));
-            setZoom(map.getZoom().toFixed(2));
-            marker.setLngLat(map.getCenter());
-            });
-        return () => map.remove();
-      }, []);
-      
-    // useEffect(()=>{
-        
-    // })
-    return(
-        <div className="map-test">
-            <div className="sidebar">
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-            </div>
-            {/* run npm install -g http-server. Next go to common/image in cmd and run http-server ./ 
+const MyMap = () => {
+  mapboxgl.workerClass = MapboxWorker;
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2h1dnJvOTciLCJhIjoiY2pubjJ4bzA1MjI4bTNxb2pwcmV4OXE4byJ9.4XJJfg7_arK7gXzDMXEpBQ";
+  const mapContainer = useRef(null);
+  const markRef = useRef(null);
+  const [lng, setLng] = useState(88.4509);
+  const [lat, setLat] = useState(22.6964);
+  const [zoom, setZoom] = useState(18);
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/shuvro97/ckn27edc22thl17ms9epngrtx",
+      center: [lng, lat],
+      zoom: zoom,
+      maxZoom: 19,
+    });
+    const marker = new mapboxgl.Marker(markRef.current)
+      .setLngLat([88.4509, 22.6964])
+      .addTo(map);
+    new mapboxgl.Marker().setLngLat([88.4509, 22.6964]).addTo(map);
+    map.on("move", () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+      marker.setLngLat(map.getCenter());
+    });
+    return () => map.remove();
+  }, []);
+
+  // useEffect(()=>{
+
+  // })
+  return (
+    <div className="map-test">
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      {/* run npm install -g http-server. Next go to common/image in cmd and run http-server ./ 
             replace below url with the one you get */}
-            <div className="map-container" ref={mapContainer} />
-            <img src="http://192.168.1.8:8080/marker.png" id="marker" ref={markRef}/>
-        </div>
-    )
-}
+      <div className="map-container" ref={mapContainer} />
+      <img src={markerimg} id="marker" ref={markRef} />
+    </div>
+  );
+};
 export default MyMap;
