c926c4f11554b5163ffa2ccc0b4eb7d8d5354494
Shubhajyoti Paul <paulshubhajyoti@gmail.com>
2021-09-12 20:45:43 +0530

    only one map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import Button from "./components/Button/Button";
-import MyMap from "./components/Button/Map2";
+// import Button from "./components/Button/Button";
+// import MyMap from "./components/Button/Map2";
 import MapPointSelect from "./components/Button/MapPointSelect";
-import TestService from "./services/TestService";
+// import TestService from "./services/TestService";
 // import RideInputs from "./components/Button/RideInputs";
 function App() {
   return (
     <div id="test">
       <div className="App">
       <h1>Casual Carpool</h1>
-      <Button name="Get All" clickHandler={TestService.getAll} />
+      {/* <Button name="Get All" clickHandler={TestService.getAll} />
       <Button name="Get By Id" clickHandler={TestService.getById} />
       <Button name="Add Value" clickHandler={TestService.addValue} />
       <Button name="Update Value" clickHandler={TestService.updateValue} />
-      <Button name="Delete Value" clickHandler={TestService.deleteValue} />
+      <Button name="Delete Value" clickHandler={TestService.deleteValue} /> */}
     </div>
     {/* <RideInputs/> */}
-    <MyMap />
+    {/* <MyMap /> */}
     <MapPointSelect/>
     </div>
   );
